7d87b1e1e236f9275c48851b21ca8729caaf81f1
unknown <hamdinassira3@gmail.com>
2019-12-25 22:18:26 +0100

    “first-commit”

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  itemCount: number=4;
+  btnTexte: string='Add an item ';
+  goalText: string='My first life goal';
+  goals=[];
 
   constructor() { }
 
   ngOnInit() {
+  }
+  addItem(){
+    this.goals.push(this.goalText);
+    this.goalText='';
+    this.itemCount=this.goals.length;
   }
 
 }
